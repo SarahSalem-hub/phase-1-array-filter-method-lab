359f33d99aea20ac4c0fbc6594fcd7a5c1a5c8f7
Sarah Al-Dhaferi <55980263+SarahSalem-hub@users.noreply.github.com>
2022-12-26 23:36:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+function findMatching(drivers,string){
+   return drivers.filter(function(one){
+    return one.toLowerCase() === string.toLowerCase()
+   })
+}
+function fuzzyMatch(drivers,string){
+
+    if (string.length === 2){
+
+    return drivers.filter(function(one){
+        if (one[0].toLowerCase() === string[0].toLowerCase() &
+        one[1].toLowerCase()  === string[1].toLowerCase()){
+            return one
+        }
+    })}
+    else return []
+    
+}
+function matchName(drivers, string ){
+
+     return drivers.filter(function(one){
+          return one.name === string 
+    })
+}
+
+ 
